1f44c7f183dfe7bf248b5d7ff7a367dd69fa787f
Ted Young <tyoung@pivotallabs.com>
2014-10-20 11:17:50 -0700

    Add dropsonde autowire to logging middleware
    Signed-off-by: Max Brunsfeld <mbrunsfeld@pivotal.io>

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 
 	"github.com/cloudfoundry-incubator/receptor"
+	"github.com/cloudfoundry/dropsonde/autowire"
 	"github.com/goji/httpauth"
 	"github.com/pivotal-golang/lager"
 )
 
 func LogWrap(handler http.Handler, logger lager.Logger) http.HandlerFunc {
+	handler = autowire.InstrumentedHandler(handler)
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		requestLog := logger.Session("request", lager.Data{
 			"method":  r.Method,
