215e65355f9c6814badcdb6d77ddf4463b822d28
Alex Suraci <suraci.alex@gmail.com>
2014-12-30 17:53:06 -0800

    wip it good

--- event/hub.go
+++ event/hub.go
@@ -1,16 +1,88 @@
 package event
 
+import (
+	"errors"
+	"sync"
+)
+
+const MAX_PENDING_SUBSCRIBER_EVENTS = 1024
+
+var ErrSlowConsumer = errors.New("slow consumer")
+var ErrReadFromClosedSource = errors.New("read from closed source")
+
+//go:generate counterfeiter -o eventfakes/fake_hub.go . Hub
 type Hub interface {
-	Subscribe() (EventSource, error)
+	Emit(Event)
+	Subscribe() EventSource
 }
 
-type EventSource interface {
-	Next() (Event, error)
-	Close() error
+type hub struct {
+	subscribers  map[chan Event]*hubSource
+	subscribersL sync.Mutex
 }
 
-type Event interface {
-	EventType() EventType
+func NewHub() Hub {
+	return &hub{
+		subscribers: make(map[chan Event]*hubSource),
+	}
 }
 
-type EventType string
+func (hub *hub) Subscribe() EventSource {
+	ch := make(chan Event, MAX_PENDING_SUBSCRIBER_EVENTS)
+
+	source := &hubSource{
+		events: ch,
+
+		unsubscribe: func() {
+			hub.subscribersL.Lock()
+			delete(hub.subscribers, ch)
+			hub.subscribersL.Unlock()
+
+			close(ch)
+		},
+	}
+
+	hub.subscribersL.Lock()
+	hub.subscribers[ch] = source
+	hub.subscribersL.Unlock()
+
+	return source
+}
+
+func (hub *hub) Emit(event Event) {
+	hub.subscribersL.Lock()
+
+	for sub, source := range hub.subscribers {
+		select {
+		case sub <- event:
+		default:
+			source.err = ErrSlowConsumer
+			close(sub)
+		}
+	}
+
+	hub.subscribersL.Unlock()
+}
+
+type hubSource struct {
+	events <-chan Event
+	err    error
+
+	unsubscribe func()
+
+	lock sync.Mutex
+}
+
+func (source *hubSource) Next() (Event, error) {
+	e, ok := <-source.events
+	if !ok {
+		return nil, source.err
+	}
+
+	return e, nil
+}
+
+func (source *hubSource) Close() {
+	source.err = ErrReadFromClosedSource
+	source.unsubscribe()
+}
--- handlers/event_stream_handler.go
+++ handlers/event_stream_handler.go
@@ -1,1 +1,58 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cloudfoundry-incubator/receptor/event"
+	"github.com/pivotal-golang/lager"
+	"github.com/vito/go-sse/sse"
+)
+
+type EventStreamHandler struct {
+	hub    event.Hub
+	logger lager.Logger
+}
+
+func NewEventStreamHandler(hub event.Hub, logger lager.Logger) *EventStreamHandler {
+	return &EventStreamHandler{
+		hub:    hub,
+		logger: logger.Session("event-stream-handler"),
+	}
+}
+
+func (h *EventStreamHandler) EventStream(w http.ResponseWriter, req *http.Request) {
+	logger := h.logger.Session("sink")
+
+	flusher := w.(http.Flusher)
+
+	source := h.hub.Subscribe()
+
+	w.WriteHeader(http.StatusOK)
+
+	flusher.Flush()
+
+	for {
+		e, err := source.Next()
+		if err != nil {
+			logger.Error("failed-to-get-next-event", err)
+			return
+		}
+
+		payload, err := json.Marshal(e)
+		if err != nil {
+			logger.Error("failed-to-marshal-event", err)
+			return
+		}
+
+		err = sse.Event{
+			Name: string(e.EventType()),
+			Data: payload,
+		}.Write(w)
+		if err != nil {
+			break
+		}
+
+		flusher.Flush()
+	}
+}
