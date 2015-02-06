ef63ad73ccab494b1be88dcbba9907195699ee30
Alex Suraci <asuraci@pivotal.io>
2015-02-05 17:41:45 -0800

    pass reconfigurable sink to cf-debug-server [#86357448]
    Signed-off-by: Ashvin Agrawal <ashvin@pivotal.io>

--- cmd/receptor/main.go
+++ cmd/receptor/main.go
@@ -126,7 +126,7 @@ func main() {
 
 	cf_http.Initialize(*communicationTimeout)
 
-	logger := cf_lager.New("receptor")
+	logger, reconfigurableSink := cf_lager.New("receptor")
 	logger.Info("starting")
 
 	initializeDropsonde(logger)
@@ -171,7 +171,7 @@ func main() {
 
 	if dbgAddr := cf_debug_server.DebugAddress(flag.CommandLine); dbgAddr != "" {
 		members = append(grouper.Members{
-			{"debug-server", cf_debug_server.Runner(dbgAddr)},
+			{"debug-server", cf_debug_server.Runner(dbgAddr, reconfigurableSink)},
 		}, members...)
 	}
 
