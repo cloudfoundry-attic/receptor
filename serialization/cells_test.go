e71b10800280f3509beebaf7291fd02b5c0b4bdf
Andrew Edgar and Michael Fraenkel <fraenkel@us.ibm.com>
2015-06-30 11:36:57 -0700

    CellResponse now includes RootFSProviders
    [#93846128]

--- serialization/cells_test.go
+++ serialization/cells_test.go
@@ -15,7 +15,7 @@ var _ = Describe("CellPresence Serialization", func() {
 
 		BeforeEach(func() {
 			capacity := models.NewCellCapacity(128, 1024, 6)
-			cellPresence = models.NewCellPresence("cell-id-0", "1.2.3.4", "the-zone", capacity)
+			cellPresence = models.NewCellPresence("cell-id-0", "1.2.3.4", "the-zone", capacity, "provider-1", "provider-2")
 		})
 
 		It("serializes all the fields", func() {
@@ -27,6 +27,7 @@ var _ = Describe("CellPresence Serialization", func() {
 					DiskMB:     1024,
 					Containers: 6,
 				},
+				RootFSProviders: []string{"provider-1", "provider-2"},
 			}
 
 			actualResponse := serialization.CellPresenceToCellResponse(cellPresence)
